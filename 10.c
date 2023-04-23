3beae577c05662914e52c70c992649505721cc9f
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-23 23:30:50 +0530

    pattern loops in c

---
+++ 10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char i, j;
+    for(i =65; i<=70; i++)
+    {
+        printf("\n");
+        for(int j=65; j<=i;j++)
+        printf("%c",j);
+
+    }return 0;
+}
