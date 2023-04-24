a2376048ace6e32b76a177d91f3d8240bb86ea6a
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-04-24 23:39:26 +0530

    asdf

---
+++ 12.c
@@ -0,0 +1,23 @@
+//Write a program to determine whether a given number is a prime or a composite number.
+#include <stdio.h>
+
+int main()
+{
+int flag = 0, i, num;
+clrscr();
+printf("\n Enter any number : ");
+scanf("%d", &num);
+for(i=2; i<num/2;i++)
+{
+if(num%i == 0)
+ {
+flag =1;
+ break;
+ }
+}
+if(flag == 1)
+printf("\n %d is a composite number", num);
+else
+printf("\n %d is a prime number", num);
+return 0;
+}
